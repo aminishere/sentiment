12f4f9c666951415a64277a15f2078ec573c7a6e
amin <106171699+aminishere@users.noreply.github.com>
2025-08-22 00:46:53 +0530

    Create install.sh
    wrote scripting to automate the installation

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Set up the project ->"
+
+#step1
+python3 -m venv venv
+source venv/bin/activate
+
+#step2
+pip install --upgrade pip
+
+#step3
+if [-f requirements.txt]; then
+    pip install -r requirements.txt
+else
+    echo "requirements.txt not found"
+    exit 1
+fi        
+
+#step4
+if [ -f sentiment_model.pkl ]; then
+    echo "model not found, first train it"
+else 
+    echo "model exists"
+fi
+
+#step5
+echo "starting the web UI"
+python app.py
